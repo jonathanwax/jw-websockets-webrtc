1f20cd378a7089d5ce2eec48100fa90c5ac2fc2c
Jonathan Wax <jonathan@netusup.com>
2015-06-15 20:59:35 +0300

    >yo jw-gn-gen app

---
+++ app/app.js
@@ -0,0 +1,34 @@
+(function () {
+    'use strict';
+
+    angular.module('app', ['ui.bootstrap', 'ui.utils', 'ui.router', 'ngAnimate', 'angular-loading-bar', 'shell', 'home', 'help', 'common']);
+
+    // CONFIG: App (module)
+    angular
+        .module('app')
+        .config(function ($stateProvider, $urlRouterProvider) {
+
+            /* Add New States Above */
+            $urlRouterProvider.otherwise('');
+
+        });
+
+    // RUN: App (module)
+    angular
+        .module('app')
+        .run(function ($rootScope) {
+
+            $rootScope.safeApply = function (fn) {
+                var phase = $rootScope.$$phase;
+                if (phase === '$apply' || phase === '$digest') {
+                    if (fn && (typeof (fn) === 'function')) {
+                        fn();
+                    }
+                } else {
+                    this.$apply(fn);
+                }
+            };
+
+        });
+
+})();
---
+++ app/common/common.js
@@ -0,0 +1,15 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('common', []);
+
+    angular
+        .module('common')
+        .config(function ($stateProvider) {
+
+            /* Add New States Above */
+
+        });
+
+})();
